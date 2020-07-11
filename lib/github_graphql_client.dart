399b9ebde5d41ce5e1d4a104c52d241831759ee0
Nick Meinhold <nick.meinhold@gmail.com>
2020-07-11 18:34:32 +1000

    Add and export all required dependencies

--- lib/github_graphql_client.dart
+++ lib/github_graphql_client.dart
@@ -1,3 +1,7 @@
 export 'src/query_exception.dart';
 export 'src/github_gql/github_queries.data.gql.dart';
 export 'src/github_gql/github_queries.req.gql.dart';
+
+export 'package:gql_exec/gql_exec.dart';
+export 'package:gql_http_link/gql_http_link.dart';
+export 'package:gql_link/gql_link.dart';
